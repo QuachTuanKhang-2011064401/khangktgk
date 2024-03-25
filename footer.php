cddf15f18f13540427032804912518891c5681b6
Dusknek <96933726+Dusknek@users.noreply.github.com>
2024-03-25 12:17:31 +0700

    ktgk

---
+++ footer.php
@@ -0,0 +1,7 @@
+</div>
+<footer>
+    &#169; 2024 - Quách Tuấn Khang
+</footer>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include_once('header.php'); ?>
+<ul>
+    <li>
+        <a href="/de_buoi1/list_nhanvien.php">Danh sách nhân viên</a>
+    </li>
+    <li>
+        <a href="/de_buoi1/add_nhanvien.php">Thêm nhân viên</a>
+    </li>
+</ul>
+<?php include_once('footer.php'); ?>
